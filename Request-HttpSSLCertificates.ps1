1488598d5c8ae625bd867a6d27e1ad29e2d1641d
Shawn Anderson <sanderson@eye-catcher.com>
2019-09-02 10:47:11 -0700

    Added sample config files Updated Request-HttpSSLCertificates.ps1 to load information from settings file

--- Request-HttpSSLCertificates.ps1
+++ Request-HttpSSLCertificates.ps1
@@ -6,18 +6,21 @@ param(
 	$forceRenewal = $false
 )
 
+Write-Host "Loading Settings file" -ForegroundColor Yellow
+$settings = (Get-Content -Raw -Path ".\settings.httpsslcertificates.json" | ConvertFrom-Json)
+
 if ($tokenPathUserName -eq $null -or $tokenPathUserName.Length -eq 0) {
-	Write-Host "Loading Settings file" -ForegroundColor Yellow
-	$settings = (Get-Content -Raw -Path ".\settings.httpsslcertificates.json" | ConvertFrom-Json)
-	$tokenFilePath = $settings.path
-	$tokenPathUserName = $settings.userName
-	$tokenPathPassword = $settings.password
+	$tokenFilePath = $settings.fileShare.path
+	$tokenPathUserName = $settings.fileShare.userName
+	$tokenPathPassword = $settings.fileShare.password
 }
 
 Write-Host "Requesting HTTP Based Certificates" -ForegroundColor Green
 Write-Host "`tFile Path: $tokenFilePath" -ForegroundColor Yellow
 Write-Host "`tUser Name: $tokenPathUserName" -ForegroundColor Yellow
-Write-Host "`tPassword: $tokenPathPassword" -ForegroundColor Yellow
+#Write-Host "`tPassword: $tokenPathPassword" -ForegroundColor Yellow
 
-.\New-PAHttpCertificate.ps1 -domainName aussierescue.org,www.aussierescue.org,beta.aussierescue.org -contactEmail certs@aussierescue.org -tokenFilePath $tokenFilePath -tokenPathUserName $tokenPathUserName -tokenPathPassword $tokenPathPassword -letsEncrypServerUrl $letsEncrypServerUrl -forceRenewal $forceRenewal
-.\New-PAHttpCertificate.ps1 -domainName spm-solutions.com,www.spm-solutions.com,beta.spm-solutions.com -contactEmail certs@liquidlogiclabs.com -tokenFilePath $tokenFilePath -tokenPathUserName $tokenPathUserName -tokenPathPassword $tokenPathPassword -letsEncrypServerUrl $letsEncrypServerUrl -forceRenewal $forceRenewal
+$settings.domains | % {
+	Write-Host "`tDomains: $($_.names)" -ForegroundColor Yellow
+	.\New-PAHttpCertificate.ps1 -domainName $_.names -contactEmail $contact -tokenFilePath $tokenFilePath -tokenPathUserName $tokenPathUserName -tokenPathPassword $tokenPathPassword -letsEncrypServerUrl $letsEncrypServerUrl -forceRenewal $forceRenewal
+}
